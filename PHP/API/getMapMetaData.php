f861daf493e95cef87ed4a2b8563376e3e50a895
florisweb <github@florisweb.tk>
2020-01-25 15:40:31 +0100

    Adjusted the getMapMetaData-script for the new system

--- PHP/API/getMapMetaData.php
+++ PHP/API/getMapMetaData.php
@@ -1,25 +1,13 @@
 <?php
 	require "APIAuthenticate.php";
+	
+	$metaData = array(
+		"compression " => $CONFIG["API"]["imageCompression"],
+		"chunkSize" => $CONFIG["world"]["mapTileSize"],
+		"miniMapList" => createMiniMapMetaData()
+	);
 
-	$metaDataList = createMiniMapMetaData();
-
-	for ($x = $CONFIG["world"]["minX"]; $x < $CONFIG["world"]["maxX"]; $x += $CONFIG["world"]["mapTileWidth"])
-	{
-		for ($z = $CONFIG["world"]["minZ"]; $z < $CONFIG["world"]["maxZ"]; $z += $CONFIG["world"]["mapTileWidth"])
-		{
-			$metaData = array(
-  				"centerX" => $x + $CONFIG["world"]["mapTileWidth"] / 2,
-  				"centerZ" => $z + $CONFIG["world"]["mapTileWidth"] / 2,
-  				"radius" => $CONFIG["world"]["mapTileWidth"] / 2,
-  				"highQuality" => false,
-          "world" => "overworld"
-  			);
-
-			array_push($metaDataList, $metaData);
-		}
-	}
-
-	echo json_encode($metaDataList);  	
+	echo json_encode($metaData);  	
 
 
 	function createMiniMapMetaData() {
@@ -35,11 +23,9 @@
 			) continue;
 
 			$metaData = array(
-				"centerX" => $project["coords"]["x"],
-				"centerZ" => $project["coords"]["z"],
-				"radius" => $project["type"]["radius"],
-				"highQuality" => true,
-        "world" => "overworld"
+				"x" => $project["coords"]["x"] - $project["type"]["radius"] / 2,
+				"z" => $project["coords"]["z"] - $project["type"]["radius"] / 2,
+				"size" => $project["type"]["radius"]
 			);
 			array_push($metaDataList, $metaData);
 		}
