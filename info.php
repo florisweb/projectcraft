3a62d4a2392fe17dda83eee274d27a6e4a78ffa8
florisweb <github@florisweb.tk>
2020-02-28 15:02:30 +0100

    Started working on the info.php's 'book'-page

--- info.php
+++ info.php
@@ -6,6 +6,190 @@
 <html>
     <head>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' name='viewport'/>
+        <link rel="stylesheet" type="text/css" href="css/main.css?antiCache=6">
+        <title><?php echo $CONFIG["server"]["name"] . " - Info"; ?></title>
+        <script type="text/javascript" src="js/jQuery.js"></script>
+
+
+        <style>
+
+            #blogHolder {
+                position: relative;
+                top: 20px;
+                margin: auto;
+                
+                width: 80vw;
+                height: 100vh;
+
+                color: #333;
+            }
+
+            #blogHolder .page {
+                position: relative;
+                float: left;
+                width: calc(40vw - 60px * 2 - 2px);
+                height: calc(100vh - 60px * 2);
+                
+                background: rgba(40, 40, 42, .9);
+                padding: 60px;
+                overflow: hidden;
+                transition: all .3s;
+
+                max-height: 100vh;
+            }
+            #blogHolder .page.hide {
+                /*transform: rotateY(85deg) translateX(-50%);*/
+                display: none;
+
+            }
+
+            #blogHolder .page:nth-child(2n - 1) {
+                background: url("images/bookPageLeft.png");
+                background-repeat: no-repeat;
+                background-size: 100% auto;
+            }
+
+            #blogHolder .page:nth-child(2n) {
+                margin-left: -10px;
+                background: url("images/bookPageRight.png");
+                background-repeat: no-repeat;
+                background-size: 100% auto;
+            }
+
+           /* #blogHolder .page:nth-child(2n):not(.hide) {
+
+            }*/
+
+
+                #blogHolder a {
+                    line-height: 20px;
+                    opacity: .8;
+                }
+
+            .blogText {
+                /*color: #333;*/
+            }
+        </style>
+    </head>
+
+    <body class="noselect">
+        <div id="homeScreen">
+            <div class="background" style="
+            background-image: url(<?php
+                $files = glob("uploads/images/*");
+                $length = sizeof($files);
+                $index = rand(0, $length - 1);
+                echo $files[$index];
+            ?>)"></div>
+
+           <div id="blogHolder" class="text">
+    
+                <?php
+                    // $descriptionLines = explode("\n", $CONFIG["server"]["description"]);
+                    $data = file_get_contents("uploads/testBlog.html");
+                    $charsPerPage = 1000;
+                    for ($i = 0; $i < strlen($data) / $charsPerPage; $i++)
+                    {
+                        $text = substr($data, $charsPerPage * $i, $charsPerPage);
+                        echo '<div class="page hide">' . $text . '</div>';
+                    }
+                    
+
+                    // for ($l = 0; $l < sizeof($descriptionLines); $l++)
+                    // {
+                    //     echo '<br><div class="text blogText" style="animation-delay: ' . $l / 4 . 's">' .
+                    //         $descriptionLines[$l] .
+                    //     '</div>';
+                    // }
+                ?>
+
+            
+                <div class="page hide">
+                    <div id='info_memberHolder'>
+                        <?php
+                            $c = 0;
+                            $members = json_decode(file_get_contents($CONFIG["memberData-url"]), true);
+                            foreach($members as $player) {
+                                echo    "<div class='avatarHolder'>" . 
+                                            "<img src='PHP/heads.php?type=body&scale=10&username=" . $player[0] . "'' class='avatar'>" . 
+                                            "<div class='text'>" . $player[0] . "</div>" . 
+                                        "</div>";
+                                $c++;
+                            }
+                        ?>
+                    </div>
+                </div>
+            
+
+
+           </div>
+        </div>
+        
+        <script type="text/javascript">
+            window.onload = function() {
+                if (getCookie("visit") != "") document.body.classList.add("quickStartAnimation");
+                document.cookie = "visit=true";
+            }
+            
+            function getCookie(cname) {
+                var name = cname + "=";
+                var ca = document.cookie.split(';');
+                for(var i = 0; i < ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0) == ' ') {
+                        c = c.substring(1);
+                    }
+                    
+                    if (c.indexOf(name) == 0) {
+                        return c.substring(name.length, c.length);
+                    }
+                }
+                return "";
+            }
+
+
+            let Page = new function() {
+                let This = {
+                    curPage: 0,
+                    openPage: openPage
+                };
+                const HTML = {
+                    pages: $("#blogHolder .page")
+                }
+
+
+                function openPage(_index) {
+                    let curPages = $("#blogHolder .page:not(.hide)");
+                    for (page of curPages) page.classList.add("hide");
+
+                    HTML.pages[_index * 2].classList.remove("hide");
+                    HTML.pages[_index * 2 + 1].classList.remove("hide");
+                }
+
+
+
+                return This;
+            }
+        </script>
+    </body>
+</html>
+
+
+
+
+<!-- 
+
+
+
+
+<?php
+    include "PHP/config.php";
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' name='viewport'/>
         <link rel="stylesheet" type="text/css" href="css/main.css?a=16">
         <title><?php echo $CONFIG["server"]["name"] . " | Info"; ?></title>
     </head>
@@ -28,34 +212,9 @@
             <?php echo strtoupper($CONFIG["server"]["name"]); ?>
         </div>
         
-        <?php
-            $descriptionLines = explode("\n", $CONFIG["server"]["description"]);
-
-            for ($l = 0; $l < sizeof($descriptionLines); $l++)
-            {
-                echo '<br><div class="text-only homescreen_projectCraftInfo" style="animation-delay: ' . $l / 4 . 's">' .
-                    $descriptionLines[$l] .
-                '</div>';
-            }
-        ?>
-
-    
-        
-        <div id='info_memberHolder'>
-            <?php
-                $c = 0;
-                foreach($CONFIG["server"]["members"] as $player) {
-                    echo    "<div class='avatarHolder'>" . 
-                                "<img src='PHP/heads.php?type=body&scale=10&username=" . $player[0] . "'' class='avatar'>" . 
-                                "<div class='text'>" . $player[0] . "</div>" . 
-                            "</div>";
-                    $c++;
-                }
-            ?>
-        </div>
-        
+      
         <div class="text-only homescreen_projectCraftInfo" style="position: relative; left: 0px; animation-delay: 15s; bottom: 0px; margin-top: 20px;">
             <a class="discord" href="<?php echo $CONFIG["server"]["discordLink"]; ?>">Come join us on our discord server.</a>
         </div>
     </body>
-</html>
+</html> -->
